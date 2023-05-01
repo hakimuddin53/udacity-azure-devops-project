cd55922f553edeb15eeeece1748ade4667def9f9
hakimuddin53 <80934027+hakimuddin53@users.noreply.github.com>
2023-05-01 14:43:36 +0800

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -10,10 +10,8 @@ def teardown_function(function):
     print("Running Teardown: %s" % function.__name__)
     del function.x
 
-
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
+def test_hello_add():
+    assert add(test_hello_add.x) == 11
 
 def test_hello_subtract():
     assert subtract(test_hello_subtract.x) == 9
